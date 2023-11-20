e3c8b28513bd90df1335957df2ee7870d8bd50a7
Полина <polina_kazhina@mail.ru>
2023-11-20 21:22:16 +0300

    fix: fix 404 test page

--- src/__test__/Page404.test.tsx
+++ src/__test__/Page404.test.tsx
@@ -1,19 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { BrowserRouter, MemoryRouter } from 'react-router-dom';
-import ErrorButton from '../components/ErrorBoundary/ErrorButton';
-import ErrorPage from '../components/ErrorBoundary/ErrorPage';
+import { MemoryRouter } from 'react-router-dom';
 import App from '../App';
+jest.mock('react-redux');
 
 describe('404 page test', () => {
-  it('Render App page', async () => {
-    render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    );
-    expect(screen.getByText(/Reset ♻/));
-  });
   it('Render 404 Page with wrong path', async () => {
     const wrongPath = '/somethingwrong/';
     render(
@@ -22,17 +13,5 @@ describe('404 page test', () => {
       </MemoryRouter>
     );
     expect(screen.getByText(/Oops!/)).toBeInTheDocument();
-  });
-  it('Render error btn', async () => {
-    render(<ErrorButton />);
-    expect(screen.getByText(/Error Button/)).toBeInTheDocument();
-  });
-  it('Render 404 Page with wrong path', async () => {
-    const wrongPath = '/somethingwrong/';
-    render(
-      <MemoryRouter initialEntries={[wrongPath]}>
-        <ErrorPage />
-      </MemoryRouter>
-    );
   });
 });
